97b969da7792344d1014601ffcf667fee58c5fbb
russdc88 <russdc88@gmail.com>
2020-01-30 11:45:46 -0700

    Adding content for section 1

--- src/components/firstSection.vue
+++ src/components/firstSection.vue
@@ -1,8 +1,22 @@
 <template>
-  <div class="section-1">
-		<h1 class="text-center" id="test">Test</h1>
-		<h2 class="text-center" id="test2">Test again</h2>
-	</div>
+	<v-container class="section-1"
+	fill-height
+	fluid
+	>
+		<v-row 
+	align="center"
+	justify="center">
+			<v-col class="text-center" id="profile-image" cols="5">
+				<v-img class="img-circle"
+				src='https://dummyimage.com/400x400/124/fff.png&text=Test'>
+				</v-img>
+			</v-col>
+			<v-col class="text-center" cols="5" id="profile-content">
+				<h2>This is a Test</h2>
+				<h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni molestiae quisquam similique ducimus? Voluptas tempore, mollitia reprehenderit praesentium, similique natus sint, repellat nemo consectetur cum vero placeat. Voluptatum, ad facere.</h5>
+			</v-col>
+		</v-row>
+</v-container>
   
 </template>
 
@@ -26,17 +40,18 @@ export default {
 		var firstScroll = new TimelineMax();
 		
 
-		firstScroll.add([
-			TweenMax.fromTo("#test", 3, {x:100,opacity:0},{x:0,opacity:1}),
-			TweenMax.fromTo("#test2", 3, {x:100,opacity:0},{x:0,opacity:1})
-		])
+		firstScroll.set("#profile-image",{x:"-100%", opacity:0})
+		firstScroll.set("#profile-content", {x:"100%", opacity:0})
+		firstScroll.to(["#profile-image","#profile-content"], 3, {x:"0%", opacity:1}
+		)
+		firstScroll.to("#profile-image", 2, {scale:.5})
 
 		var controller = new ScrollMagic.Controller()
 
 		var containerScene = new ScrollMagic.Scene({
 		triggerElement: '.section-1',
 		triggerHook: 0.4,
-		duration: 300
+		duration: 400
 })
 	.setTween(firstScroll)
 	.addTo(controller);
@@ -52,12 +67,8 @@ export default {
 	.section-1 {
 		height: 700px; 
 	}
-
-	h1 {
-		padding-top: 300px;
+	.img-circle {
+		border-radius: 50%;
 	}
 
-	#test {
-		opacity: 0;
-	}
 </style>
